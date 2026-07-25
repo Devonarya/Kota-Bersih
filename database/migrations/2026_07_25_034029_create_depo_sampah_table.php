6e540c3f2a0e7674f56494606740b2f314e3babe
Devonarya <devonaryadaniswra@gmail.com>
2026-07-25 11:18:08 +0700

    menambah tabel depo sampah

--- database/migrations/2026_07_25_034029_create_depo_sampah_table.php
+++ database/migrations/2026_07_25_034029_create_depo_sampah_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('depo_sampah', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('banjar_id')->constrained()->cascadeOnDelete();
+            $table->enum('jenis_sampah', ['organik', 'anorganik', 'Kaca', 'b3']);
+            $table->text(keterangan)->nullable();
+            $table->enum('status', ['pending', 'diterima', 'ditolak', 'selesai'])->default('selesai');
+            $table->foreignId('pengangku_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->date('scheduled_date')->nullable();
+            $table->string('schedule_time_slot')->nullable();
+            $table->date('deposisted_on');
             $table->timestamps();
         });
     }
